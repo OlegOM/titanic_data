d6d443a47cb4147ab755ae744d123004c6b55f30
Oleg Miroshnichenko <oleg.dev123@gmail.com>
2023-08-31 21:10:00 +0300

    small beauty refacgtor

--- app/app.py
+++ app/app.py
@@ -33,6 +33,7 @@ conn = sqlite3.connect('titanic.db')
 # Asynchronous route to return a histogram of Fare prices in percentiles
 @app.get('/get_histogram')
 async def get_histogram():
+
     fares = [float(row['Fare']) for row in data]
     percentiles = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     prices_percentiles = {percent: np.percentile(fares, percent) for percent in percentiles}
@@ -42,6 +43,7 @@ async def get_histogram():
 # Asynchronous route to return all passenger data in JSON format for a given PassengerId
 @app.get('/get_passenger/{passenger_id}')
 async def get_passenger(passenger_id: int):
+    
     # Find the passenger by PassengerId
     passenger = next((p for p in data if int(p['PassengerId']) == passenger_id), None)
 
@@ -54,8 +56,9 @@ async def get_passenger(passenger_id: int):
 # Asynchronous route to return requested attributes in JSON format for a given PassengerId
 @app.get('/get_passenger_attributes/{passenger_id}')
 async def get_passenger_attributes(
-    passenger_id: int, attributes: List[str] = Query(..., description="List of attributes to retrieve")
-):
+    passenger_id: int,
+    attributes: List[str] = Query(..., description="List of attributes to retrieve")):
+
     passenger = next((p for p in data if int(p['PassengerId']) == passenger_id), None)
     if not passenger:
         raise HTTPException(status_code=404, detail="Passenger not found")
